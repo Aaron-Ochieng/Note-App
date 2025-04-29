af84e1153fc23e4fa506ec365445ba449992dc4e
Aaron Ochieng <ochiengaoron@outlook.com>
2025-04-29 20:38:26 +0300

    add: designed note appbar with search options

--- app/src/main/java/com/example/notes/screens/appbar.kt
+++ app/src/main/java/com/example/notes/screens/appbar.kt
@@ -1,2 +1,95 @@
 package com.example.notes.screens
 
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true)
+@Composable
+private fun AppBarPreview() {
+    val search = remember { mutableStateOf(false) }
+    val  searchVal = remember { mutableStateOf("") }
+    AppBar(search = search, searchText = searchVal)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar(
+    search: MutableState<Boolean>,
+    searchText : MutableState<String>
+) {
+    CenterAlignedTopAppBar(
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.background,
+            navigationIconContentColor = Color.Black,
+            titleContentColor = Color.Black,
+            actionIconContentColor = Color.Black
+        ),
+        title = {
+            if (!search.value){
+                Text("All Notes")
+            }else{
+                    TextField(
+                        value = searchText.value,
+                        onValueChange = { searchText.value = it },
+                        placeholder = { Text("Search...") },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth(),
+                        colors = TextFieldDefaults.colors(
+                            focusedContainerColor = MaterialTheme.colorScheme.background,
+                            unfocusedContainerColor = MaterialTheme.colorScheme.background,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            focusedTextColor = Color.Black
+                        ),
+                    )
+            }
+        },
+        actions = {
+            IconButton(
+                onClick = {
+                    search.value = !search.value
+                    if (!search.value){
+                        searchText.value = ""
+                    }
+                }
+            ) {
+                if (!search.value){
+                    Icon(
+                        imageVector =  Icons.Default.Search,
+                        contentDescription = null
+                    )
+                }else{
+                    Icon(
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = null
+                    )
+                }
+
+            }
+        }
+    )
+}
