fbd0c671f61d79a09d48d8cc654942c3a348a6cf
Aaron Ochieng <ochiengaoron@outlook.com>
2025-04-29 21:35:43 +0300

    add: app routes

--- app/src/main/java/com/example/notes/navigator.kt
+++ app/src/main/java/com/example/notes/navigator.kt
@@ -1,2 +1,14 @@
 package com.example.notes
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+
+sealed class Screen(val route : String) {
+    object Home : Screen("home")
+    object New : Screen("new")
+    object Edit : Screen("edit/{id}") {
+        fun createRoute(id: Int) = "edit/$id"
+    }
+}
+
