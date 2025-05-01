b31494f46a9e3d2a8e4bfa7b098a815905d58528
Aaron Ochieng <ochiengaoron@outlook.com>
2025-05-01 15:16:13 +0300

    Refractor: fromColor object - Now picks random colors from given / list of provided colors

--- app/src/main/java/com/example/notes/data/Note.kt
+++ app/src/main/java/com/example/notes/data/Note.kt
@@ -1,14 +1,28 @@
 package com.example.notes.data
 
-import kotlinx.serialization.Serializable
 
-@Serializable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import com.example.notes.ui.theme.LightAqua
+import com.example.notes.ui.theme.LightBrown
+import com.example.notes.ui.theme.LightGreen
+import com.example.notes.ui.theme.LightPink
+import kotlinx.datetime.LocalDateTime
+
+
 data class Note(
-    var id : Int,
+    var id: Int,
     var title: String,
-    var content : String,
-    var createdAt : Long
-)
+    var content: String,
+    var createdAt: LocalDateTime,
+    var color: Int
+) {
+    fun toColor(): Color = Color(color) // Convert Int to Color
+    companion object {
+        fun fromColor(): Int  {
+            val colors = listOf<Color>(LightPink, LightAqua, LightBrown, LightGreen)
+            return colors.random().toArgb()
+        } // Convert Color to Int
+    }
 
-
-val notes = listOf<Note>()
+}
