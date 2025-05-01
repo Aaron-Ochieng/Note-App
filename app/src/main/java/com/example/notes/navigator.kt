22b2ec7523bd98dbb32880494d45c59378123010
Aaron Ochieng <ochiengaoron@outlook.com>
2025-05-01 17:41:26 +0300

    feat: edit note route to get noteId based on the clicked note

--- app/src/main/java/com/example/notes/navigator.kt
+++ app/src/main/java/com/example/notes/navigator.kt
@@ -1,18 +1,22 @@
 package com.example.notes
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.notes.data.NoteViewModel
+import com.example.notes.screens.Edit
 import com.example.notes.screens.Home
 import com.example.notes.screens.NewNote
 
 sealed class Screen(val route : String) {
     object Home : Screen("home")
     object New : Screen("new")
-    object Edit : Screen("edit")
+    object Edit : Screen("edit/{noteId}")
 }
 
 @Composable
@@ -30,8 +34,23 @@ fun AppNavigation(viewModel: NoteViewModel) {
                 navController = navController
             )
         }
-        composable(Screen.Edit.route) {
-
+        composable(
+            Screen.Edit.route,
+            arguments = listOf(navArgument("noteId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val noteId = backStackEntry.arguments?.getInt("noteId")
+            val note = notes.value?.find { it.id == noteId }
+            if (noteId != null) {
+                Edit(
+                    noteId = noteId,
+                    navController = navController,
+                    viewModel = viewModel,
+                    note = note
+                )
+            } else {
+                // Handle the case where noteId is null (shouldn't happen if navigation is correct)
+                Text("Error: Note ID not found")
+            }
         }
         composable(Screen.New.route) {
             NewNote(navController = navController, viewModel = viewModel)
