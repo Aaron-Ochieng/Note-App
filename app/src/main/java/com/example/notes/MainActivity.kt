d87204874f53a6e484d098bc048c4acdcb92687e
Aaron Ochieng <ochiengaoron@outlook.com>
2025-05-01 15:27:12 +0300

    feat: Setup MainActivity for Note Application
    - Configured edge-to-edge display for a modern look.
    - Configured status bar text and icons to black.
    - Implemented the UI using Jetpack Compose, with NotesTheme and Surface.
    - Implemented AppNavigation composable function to handle navigation between different screens.
    - Instantiated and pass the NoteViewModel to AppNavigation to manage the note data.

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.notes.data.NoteViewModel
+import com.example.notes.screens.AppBar
 import com.example.notes.ui.theme.NotesTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,31 +31,13 @@ class MainActivity : ComponentActivity() {
 
         // Set the status bar icons to black
         WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = true
+        val notesViewModel = ViewModelProvider(this)[NoteViewModel::class.java]
         setContent {
             NotesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface {
+                    AppNavigation(notesViewModel)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NotesTheme {
-        Greeting("Android")
     }
 }
