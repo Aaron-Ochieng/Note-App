5e7aa001ae38a96efd503ceb1e8f93ebc23af30b
Aaron Ochieng <ochiengaoron@outlook.com>
2025-05-01 15:17:54 +0300

    Feat: Navigation manager function -Added home,edit and new note route

--- app/src/main/java/com/example/notes/navigator.kt
+++ app/src/main/java/com/example/notes/navigator.kt
@@ -1,20 +1,40 @@
 package com.example.notes
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.notes.data.NoteViewModel
+import com.example.notes.screens.Home
+import com.example.notes.screens.NewNote
 
 sealed class Screen(val route : String) {
     object Home : Screen("home")
     object New : Screen("new")
-    object Edit : Screen("edit/{id}") {
-        fun createRoute(id: Int) = "edit/$id"
-    }
+    object Edit : Screen("edit")
 }
 
 @Composable
-fun AppNavigation() {
+fun AppNavigation(viewModel: NoteViewModel) {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = Screen.Home.route){
+    val notes =  viewModel.notesList.observeAsState(initial = null)
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ){
+        composable(Screen.Home.route){
+            Home(
+                notes = notes.value ?: emptyList(),
+                navController = navController
+            )
+        }
+        composable(Screen.Edit.route) {
+
+        }
+        composable(Screen.New.route) {
+            NewNote(navController = navController, viewModel = viewModel)
+        }
     }
 }
