cce716bea6d91f4c662b9f46d496948f560276ce
Aaron Ochieng <ochiengaoron@outlook.com>
2025-05-01 15:26:18 +0300

    add Home screen appbar

--- app/src/main/java/com/example/notes/screens/appbar.kt
+++ app/src/main/java/com/example/notes/screens/appbar.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.notes.data.Note
 
 @Preview(showBackground = true)
 @Composable
