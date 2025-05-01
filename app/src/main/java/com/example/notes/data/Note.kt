07798a9781f948946d28af48ff812976a1f86634
Aaron Ochieng <ochiengaoron@outlook.com>
2025-05-01 21:28:40 +0300

    Add: Room Entity data class - Added @Entity anotation and PrimaryKey

--- app/src/main/java/com/example/notes/data/Note.kt
+++ app/src/main/java/com/example/notes/data/Note.kt
@@ -3,6 +3,8 @@ package com.example.notes.data
 
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.notes.ui.theme.LightAqua
 import com.example.notes.ui.theme.LightBrown
 import com.example.notes.ui.theme.LightGreen
@@ -10,8 +12,10 @@ import com.example.notes.ui.theme.LightPink
 import kotlinx.datetime.LocalDateTime
 
 
+@Entity
 data class Note(
-    var id: Int,
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
     var title: String,
     var content: String,
     var createdAt: LocalDateTime,
