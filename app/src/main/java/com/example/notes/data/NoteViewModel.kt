43d4af3967d1afee8fffbca952bbf3a59c201f55
Aaron Ochieng <ochiengaoron@outlook.com>
2025-05-01 15:24:08 +0300

    Feat: Introduce NoteViewModel to handle note data for UI - NoteViewModel, a ViewModel responsible for interacting with the UI layer regarding note data.
    Key features:
    - Manages a list of notes using LiveData for observation by UI elements.
    - Provides methods for creating, updating, and deleting notes, delegating the operations to NoteManager.
    - The notesList LiveData automatically updates the UI when changes occur.
    - Handles the logic to fetch notes from the NoteManager.

--- app/src/main/java/com/example/notes/data/NoteViewModel.kt
+++ app/src/main/java/com/example/notes/data/NoteViewModel.kt
@@ -1,4 +1,29 @@
 package com.example.notes.data
 
-class NoteViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NoteViewModel : ViewModel() {
+    private var _notes = MutableLiveData<List<Note>>()
+    val notesList : LiveData<List<Note>> = _notes
+
+
+
+    fun getNotes (){
+        _notes.value = NoteManager.getNotes()
+    }
+
+
+    fun addNote (title: String, content: String){
+        NoteManager.addNote(title,content)
+        getNotes()
+    }
+
+    fun editNote(id: Int,title: String,content: String){
+        NoteManager.editNote(id = id,title = title,content=content)
+    }
+    fun deleteNote (id: Int){
+        NoteManager.deleteNote(id)
+    }
 }
