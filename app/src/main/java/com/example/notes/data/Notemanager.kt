cdc09c3b753b1bc2ec50468c7d10d5aa602ed511
Aaron Ochieng <ochiengaoron@outlook.com>
2025-05-01 15:22:09 +0300

    Add NoteManager for CRUD operations on notes - `getNotes()`: Retrieves the list of all notes. - `addNote(title, content)`: Creates a new note with the provided title and content, assigning a unique ID and setting the creation timestamp. - `editNote(id, title, content)`: Updates the title and content of an existing note with the specified ID. - `deleteNote(id)`: Removes the note with the given ID from the list.

--- app/src/main/java/com/example/notes/data/Notemanager.kt
+++ app/src/main/java/com/example/notes/data/Notemanager.kt
@@ -1,4 +1,38 @@
 package com.example.notes.data
 
-object Notemanager {
+
+
+object NoteManager {
+    private val notes = mutableListOf<Note>()
+
+    fun getNotes () : List<Note>{
+        return notes
+    }
+
+
+    fun addNote (title : String, content : String){
+        notes.add(
+            Note(
+                id = System.currentTimeMillis().toInt(),
+                title = title,
+                content = content,
+                createdAt = DateManager.now(),
+                color = Note.fromColor()
+            )
+        )
+    }
+
+    fun editNote (id : Int, title : String, content : String){
+        notes.forEach {
+            if (it.id == id) {
+                it.title = title
+                it.content = content
+            }
+        }
+    }
+    fun deleteNote (id : Int){
+        notes.removeIf{
+            it.id == id
+        }
+    }
 }
