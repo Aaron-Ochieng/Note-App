b3be471b22d8aa7b089a526adef7aa97715687bd
aaochieng <ochiengaoron@outlook.com>
2025-05-02 08:53:19 +0300

    fix: changed createdAt to be stored as string which roomdb can store

--- app/src/main/java/com/example/notes/data/Note.kt
+++ app/src/main/java/com/example/notes/data/Note.kt
@@ -18,7 +18,7 @@ data class Note(
     var id: Int = 0,
     var title: String,
     var content: String,
-    var createdAt: LocalDateTime,
+    var createdAt: String,
     var color: Int
 ) {
     fun toColor(): Color = Color(color) // Convert Int to Color
